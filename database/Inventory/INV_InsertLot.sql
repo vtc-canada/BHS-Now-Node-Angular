357a14b1446fc11522a718e6713626d7470c7bab
wiechars <rwiecha@isystemsnow.com>
2015-07-15 15:23:00 -0400

    Adjusted to use helper sprocs for updating.

--- database/Inventory/INV_InsertLot.sql
+++ database/Inventory/INV_InsertLot.sql
@@ -1,32 +1,44 @@
 DROP procedure IF EXISTS `INV_InsertLot`;
 
 DELIMITER $$
-CREATE DEFINER=`root`@`%` PROCEDURE `INV_InsertLot`(IN uomID INT(11), IN brandID INT(11), IN typeID INT(11), IN serial_no VARCHAR(128), IN quantity VARCHAR(45), IN price VARCHAR(128), IN date_added DATETIME, IN expiration_date DATETIME, IN user_name VARCHAR(128), IN notes VARCHAR(256)
-,IN tread_depth VARCHAR(128),IN side_wall VARCHAR(128),IN tire_type VARCHAR(128),IN tire_size VARCHAR(128)
-, IN is_deleted TINYINT(4), OUT insertID INT(11))
+CREATE PROCEDURE `INV_InsertLot`(IN uomID INT,IN lotStatusID INT, IN locationID INT,IN brandID INT, IN typeID INT, IN serial_no VARCHAR(128), IN quantity VARCHAR(45)
+					,IN price VARCHAR(128), IN date_added DATETIME, IN expiration_date DATETIME, IN user_name VARCHAR(128), IN notes VARCHAR(256)
+					,IN propValsID VARCHAR(255), IN propVals VARCHAR(255), OUT insertID INT)
 BEGIN
 
 
-INSERT INTO inv_cur_lots (`inv_cfg_uom_id`, `inv_cfg_mat_brands_id`, `inv_cfg_mat_types_id`, `serial_no`, `quantity`, `price`, `born_date`, `expiration_date`, `user_name`, `notes`)
-VALUES (uomID, brandID, typeID, serial_no, quantity, price, date_added, expiration_date, user_name, notes);
+	INSERT INTO inv_cur_lots (`inv_cfg_uom_id`
+								,`inv_cfg_lot_status_id`
+								,`inv_cfg_locations_id`
+								,`inv_cfg_mat_brands_id`
+								,`inv_cfg_mat_types_id`
+								,`serial_no`
+								,`quantity`
+								,`price`
+								,`born_date`
+								,`expiration_date`
+								,`user_name`
+								,`notes`
+								,`is_deleted`)
+	VALUES (uomID
+			,lotStatusID
+			,locationID
+			,brandID
+			,typeID
+			,serial_no
+			,quantity
+			,price
+			,date_added
+			,expiration_date
+			,user_name
+			,notes
+			,0);
 
 SET insertID = LAST_INSERT_ID();
+
+CALL `INV_UpdateLotCustomProps` (insertID, propValsID, propVals, user_name);
  
-SELECT @propID :=  id FROM nms.inv_cfg_custom_props where property = 'tread_depth'; 
-INSERT INTO `inv_cur_prop_vals` (`inv_cfg_custom_props_id`,`inv_cur_lots_id`,`value`)
-VALUES (@propID,insertID,tread_depth);
 
-SELECT @propID :=  id FROM nms.inv_cfg_custom_props where property = 'side_wall'; 
-INSERT INTO `inv_cur_prop_vals` (`inv_cfg_custom_props_id`,`inv_cur_lots_id`,`value`)
-VALUES (@propID,insertID,side_wall);
-
-SELECT @propID :=  id FROM nms.inv_cfg_custom_props where property = 'tire_type'; 
-INSERT INTO `inv_cur_prop_vals` (`inv_cfg_custom_props_id`,`inv_cur_lots_id`,`value`)
-VALUES (@propID,insertID,tire_type);
-
-SELECT @propID :=  id FROM nms.inv_cfg_custom_props where property = 'tire_size'; 
-INSERT INTO `inv_cur_prop_vals` (`inv_cfg_custom_props_id`,`inv_cur_lots_id`,`value`)
-VALUES (@propID,insertID,tire_size);
 
 
 END$$
--- database/Inventory/INV_UpdateLot.sql
+++ database/Inventory/INV_UpdateLot.sql
@@ -3,46 +3,21 @@ DROP procedure IF EXISTS `INV_UpdateLot`;
 
 DELIMITER $$
 
-CREATE DEFINER=`root`@`%` PROCEDURE `INV_UpdateLot`(     IN lotID INT(11), IN uomID INT(11), IN brandID INT(11), IN typeID INT(11), IN serial_no VARCHAR(128), IN quantity VARCHAR(45), IN price VARCHAR(128), IN date_added DATETIME, IN expiration_date DATETIME, IN user_name VARCHAR(128), IN notes VARCHAR(256)
-,IN tread_depth VARCHAR(128),IN side_wall VARCHAR(128),IN tire_type VARCHAR(128),IN tire_size VARCHAR(128)
-, IN is_deleted TINYINT(4)
-)
+CREATE PROCEDURE `INV_UpdateLot`(IN lotID INT(11), IN uomID INT(11),IN quantity VARCHAR(45), IN price VARCHAR(128)
+								,IN dateAdded DATETIME, IN expirationDate DATETIME,IN userName VARCHAR(128)
+								,IN propValsID VARCHAR(255), IN propVals VARCHAR(255))
 BEGIN
 	UPDATE inv_cur_lots 
 
-SET inv_cfg_uom_id = uomID
-,inv_cfg_mat_brands_id = brandID
-,inv_cfg_mat_types_id = typeID
-,serial_no = serial_no
-,quantity = quantity
-,price = price
-,born_date = date_added
-,expiration_date = expiration_date
-,user_name = user_name
-,notes = notes
-,is_deleted = is_deleted
-WHERE inv_cur_lots.id = lotID;
+	SET inv_cfg_uom_id = uomID
+		,price = price
+		,born_date = dateAdded
+		,expiration_date = expirationDate
+		,user_name = userName
+	WHERE inv_cur_lots.id = lotID;
 
-UPDATE `inv_cur_prop_vals`
-INNER JOIN `inv_cfg_custom_props` ON `inv_cur_prop_vals`.`inv_cfg_custom_props_id` = `inv_cfg_custom_props`.`id`
-AND `inv_cfg_custom_props`.`property` = 'tread_depth' AND `inv_cur_prop_vals`.`inv_cur_lots_id` = lotID
-SET `inv_cur_prop_vals`.`value` = tread_depth;
-
-UPDATE `inv_cur_prop_vals`
-INNER JOIN `inv_cfg_custom_props` ON `inv_cur_prop_vals`.`inv_cfg_custom_props_id` = `inv_cfg_custom_props`.`id`
-AND `inv_cfg_custom_props`.`property` = 'side_wall' AND `inv_cur_prop_vals`.`inv_cur_lots_id` = lotID
-SET `inv_cur_prop_vals`.`value` = side_wall;
-
-UPDATE `inv_cur_prop_vals`
-INNER JOIN `inv_cfg_custom_props` ON `inv_cur_prop_vals`.`inv_cfg_custom_props_id` = `inv_cfg_custom_props`.`id`
-AND `inv_cfg_custom_props`.`property` = 'tire_type' AND `inv_cur_prop_vals`.`inv_cur_lots_id` = lotID
-SET `inv_cur_prop_vals`.`value` = tire_type;
-
-UPDATE `inv_cur_prop_vals`
-INNER JOIN `inv_cfg_custom_props` ON `inv_cur_prop_vals`.`inv_cfg_custom_props_id` = `inv_cfg_custom_props`.`id`
-AND `inv_cfg_custom_props`.`property` = 'tire_size' AND `inv_cur_prop_vals`.`inv_cur_lots_id` = lotID
-SET `inv_cur_prop_vals`.`value` = tire_size;
-
+	CALL `INV_UpdateLotQuantity` (lotID, quantity, userName);
+	CALL `INV_UpdateLotCustomProps` (lotID, propValsID, propVals, userName);
 
 
 
