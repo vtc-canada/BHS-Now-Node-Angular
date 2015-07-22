f81a16b3892697023d233343d3d8a9b75695c937
wiechars <rwiecha@isystemsnow.com>
2015-07-22 15:58:14 -0400

    New sprocs and fixed formatting issues.

--- database/Orders/ODR_GetOrderEntries.sql
+++ database/Orders/ODR_GetOrderEntries.sql
@@ -4,17 +4,16 @@ DROP procedure IF EXISTS `ODR_GetOrderEntries`;
 DELIMITER $$
 CREATE PROCEDURE `ODR_GetOrderEntries` (IN orderId INT(11))
 BEGIN
-SELECT `id`,
-`odr_cur_orders_id`,
-`inv_cfg_mat_types_id`,
-`inv_cfg_mat_brands_id`,
-`quantity`,
-`price`,
-`inv_cfg_uom_id`,
-`last_modified`
-FROM 
-`odr_cur_order_entries`
-WHERE `odr_cur_orders_id` = orderId;
+	SELECT odr_cur_order_entries.id
+		,odr_cur_order_entries.odr_cur_orders_id
+		,odr_cur_order_entries.inv_cfg_mat_types_id
+		,odr_cur_order_entries.inv_cfg_mat_brands_id
+		,odr_cur_order_entries.quantity
+		,odr_cur_order_entries.price
+		,odr_cur_order_entries.inv_cfg_uom_id
+		,odr_cur_order_entries.last_modified
+	FROM  odr_cur_order_entries
+	WHERE odr_cur_order_entries.odr_cur_orders_id = orderId;
 END
 $$
 
