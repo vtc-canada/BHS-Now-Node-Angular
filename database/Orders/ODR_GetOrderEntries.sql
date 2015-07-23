f07490732ccaa31b38ab9d321ae6348cb2ab51c2
wiechars <rwiecha@isystemsnow.com>
2015-07-23 15:17:31 -0400

    Add more details to order entries

--- database/Orders/ODR_GetOrderEntries.sql
+++ database/Orders/ODR_GetOrderEntries.sql
@@ -8,12 +8,18 @@ BEGIN
 		,odr_cur_order_entries.odr_cur_orders_id
 		,odr_cur_order_entries.inv_cfg_mat_types_id
 		,odr_cur_order_entries.inv_cfg_mat_brands_id
-		,odr_cur_order_entries.quantity
-		,odr_cur_order_entries.price
 		,odr_cur_order_entries.inv_cfg_uom_id
+		,inv_cfg_mat_types.type AS 'mat_type'
+		,inv_cfg_mat_brands.brand AS 'brand_type'
+		,odr_cur_order_entries.quantity
+		,inv_cfg_uom.uom
 		,odr_cur_order_entries.last_modified
 	FROM  odr_cur_order_entries
+		LEFT JOIN inv_cfg_mat_types ON (odr_cur_order_entries.inv_cfg_mat_types_id = inv_cfg_mat_types.id)
+		LEFT JOIN inv_cfg_mat_brands ON (odr_cur_order_entries.inv_cfg_mat_brands_id = inv_cfg_mat_brands.id)
+		LEFT JOIN inv_cfg_uom ON (odr_cur_order_entries.inv_cfg_uom_id = inv_cfg_uom.id)
 	WHERE odr_cur_order_entries.odr_cur_orders_id = orderId;
+
 END
 $$
 
